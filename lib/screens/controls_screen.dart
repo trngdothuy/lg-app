50ce0f8db4141d852b288c5a6e3b441a2afb5a7e
trngdothuy <dothuytrang2312@gmail.com>
2026-03-21 12:01:47 +0700

    Add controls screen

--- lib/screens/controls_screen.dart
+++ lib/screens/controls_screen.dart
@@ -5,4 +5,26 @@ class ControlsScreen extends StatefulWidget {
     final SSHClient? client;
     final String host;
     final int screens;
+
+    const ControlsScreen({
+        super.key,
+        required this.client,
+        required this.host,
+        required this.screens,
+    });
+
+    @override State<ControlsScreen> createState() => _ControlsScreenState();
+}
+
+class _ControlsScreenState extends State<ControlsScreen> {
+    @override Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('LG Controller - ${widget.host}'),
+            ),
+            body: const Center(
+                child: Text('Button'),
+                ),
+        );
+    }
 }
