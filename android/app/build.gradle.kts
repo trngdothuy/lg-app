7e7ef633c7312344d6466e48c1242902ee01e4e4
trngdothuy <dothuytrang2312@gmail.com>
2026-08-18 23:37:51 +0200

    Debug to load kmls across all screens and configure apk build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
 }
